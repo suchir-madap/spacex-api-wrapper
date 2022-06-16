d860dec49b61fbd0692547b535fc725068d79033
svmadap <81061506+svmadap@users.noreply.github.com>
2022-06-16 14:49:23 -0400

    basic python v2

--- satellite.py
+++ satellite.py
@@ -6,8 +6,15 @@ import json
 def satellite_data(data_id):
     satellite = requests.get(
         f"https://api.spacexdata.com/v4/starlink/{str(data_id)}")
+    store = satellite.json()
+    wanted_values = ["CREATION_DATE", "OBJECT_NAME", "OBJECT_ID", "LAUNCH_DATE"]
+    new_dict = dict(
+        (x,y)
+        for x,y in store['spaceTrack'].items() if x in wanted_values
+    )
+    store['spaceTrack'] = new_dict
 
     with open('data.json', 'w') as outfile:
-        json.dump(satellite.json(), outfile)
+        json.dump(store, outfile, indent= 1)
 
 satellite_data('628ea116a8973c1694df189b')
