d2aedf63a504db2ebaf518d91fa1d4e3a456990b
svmadap <81061506+svmadap@users.noreply.github.com>
2022-07-04 22:38:45 -0400

    C++ start 2

--- main.cpp
+++ main.cpp
@@ -5,12 +5,6 @@ using namespace std;
 string satellite_id;
 
 
-int main() { 
-	cout << "Please type the Spacex Satellite Id Below\n";
-	
-	cin >> satellite_id; // input statement 628ea116a8973c1694df189b
-	cout << satellite_id;
-}
 
 void satellite_data() {
 	string merge = "https://api.spacexdata.com/v4/starlink/" + satellite_id;
@@ -18,3 +12,12 @@ void satellite_data() {
 	cout << r.text;
 	
 }
+
+int main() { 
+	cout << "Please type the Spacex Satellite Id Below\n";
+	
+	cin >> satellite_id; // input statement 628ea116a8973c1694df189b
+	cout << satellite_id;
+	satellite_data()
+}
+
