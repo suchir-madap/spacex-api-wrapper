f93b553707324aaa2e4fd28f441bf1d35f9a1996
svmadap <81061506+svmadap@users.noreply.github.com>
2022-07-12 11:30:34 -0400

    Spacex v3 change

--- src/kuiper.cpp
+++ src/kuiper.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 
+
 string satellite_id;
 
 
--- src/starlink.cpp
+++ src/starlink.cpp
@@ -1,15 +1,21 @@
 #include <iostream>
 #include <string>
+#include <fstream>
+#include <cstdlib>
 using namespace std; 
 string satellite_id;
-
+string data;
 // SpaceX Starlink V4.0
 int main() {
 	cout << "Please type the Spacex Satellite Id Below\n";
-	cin >> satellite_id; // input statement
+	cin >> satellite_id; // input statement 628ea116a8973c1694df189b
 
-	string merge = "curl https://api.spacexdata.com/v4/starlink/" + satellite_id;
+	string merge = "curl https://api.spacexdata.com/v4/starlink/" + satellite_id + " >> src/starlink.json";
+	// line above redirects and stores system command into the starlink.json file
 
-    system(merge.c_str()); // c_str converts the string into an array of characters
+	system(merge.c_str());// c_str converts the string into an array of characters
 	//basic linux command will return the get command for a single satellite
+
+
+
 }
